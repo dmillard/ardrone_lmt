a9c16265448f0ae6cfb327e36a4c4eed7cc57cd9
Nils Lichtenberg <nlichtenberg@nlichtenbergLap.(none)>
2013-11-14 04:38:11 +0100

    Line detection and main for line challenge

--- src/LineToLine.cpp
+++ src/LineToLine.cpp
@@ -9,8 +9,20 @@
 #include "LineToLine.h"
 
 bool show_frame = true;
+int hough_edge_thresh = 100;
+int hough_max_edge_thresh = 255;
+int hough_space_resolution = 10;
+int hough_space_resolution_max = 500;
+int hough_angle_resolution = 90;
+int hough_angle_resolution_max = 180;
+int hough_gap = 0;
+int hough_gap_max = 500;
+int hough_min_len = 30;
+int hough_min_len_max = 500;
+int min_len_factor = 7;
+std::vector<cv::Vec4i> lines;
 
-int dominance = 40;
+int dominance = 25;
 int thresh = 10; //proportion of foreground
 
 LineToLine::LineToLine() {
@@ -23,59 +35,46 @@ LineToLine::~LineToLine() {
 
 using namespace cv;
 bool LineToLine::processImg(cv::Mat src){
+	Mat src_copy = src.clone();
 	int accu = 0;
 	IplImage resultImage;
 	IplImage sourceImage;
 	IplImage skelImage;
 	float dominance_factor = (float) dominance/100 + 1;
-	cv::Mat src_gray (src.size(), CV_8U);
-	cv::Mat src_bw (src.size(), CV_8U);
-	for(int x = 0; x < src.cols; x++)
-		for(int y = 0; y < src.rows; y++){
-			if(src.data[y*src.cols*3 + x*3] > src.data[y*src.cols*3 + x*3 + 1] * dominance_factor
-			    && src.data[y*src.cols*3 + x*3] > src.data[y*src.cols*3 + x*3 + 2] * dominance_factor){
-				src_gray.data[y*src.cols + x] = 255;
+	cv::Mat src_gray (src_copy.size(), CV_8U);
+	cv::Mat src_bw (src_copy.size(), CV_8U);
+	for(int x = 0; x < src_copy.cols; x++)
+		for(int y = 0; y < src_copy.rows; y++){
+			if(src.data[y*src_copy.cols*3 + x*3] > src_copy.data[y*src.cols*3 + x*3 + 1] * dominance_factor
+			    && src.data[y*src_copy.cols*3 + x*3] > src_copy.data[y*src.cols*3 + x*3 + 2] * dominance_factor){
+				src_gray.data[y*src_copy.cols + x] = 255;
 				accu++;
 			}
 			else
 				src_gray.data[y*src.cols + x] = 0;
 		}
-	Mat skel(src.size(), CV_8UC1, cv::Scalar(0));
-	Mat temp(src.size(), CV_8UC1);
-	Mat element = cv::getStructuringElement(cv::MORPH_CROSS, cv::Size(3, 3));
-
-
-		// Apply the erosion operation
-	cv::dilate( src_gray, src_gray, element, Point(-1,-1), 10 );
-	cv::erode( src_gray, src_gray, element, Point(-1,-1), 60 );
-
-	// skeletization
-//	Mat gray_temp = src_gray.clone();
-//	bool done = false;
-//	while(!done)
-//	{
-//	  cv::morphologyEx(gray_temp, temp, cv::MORPH_OPEN, element);
-//	  cv::bitwise_not(temp, temp);
-//	  cv::bitwise_and(gray_temp, temp, temp);
-//	  cv::bitwise_or(skel, temp, skel);
-//	  cv::erode(gray_temp, gray_temp, element);
-//
-//	  double max;
-//	  cv::minMaxLoc(gray_temp, 0, &max);
-//	  done = (max == 0);
-//	}
+	//Mat skel(src.size(), CV_8UC1, cv::Scalar(0));
+	//Mat temp(src.size(), CV_8UC1);
+	//Mat element = cv::getStructuringElement(cv::MORPH_CROSS, cv::Size(3, 3));
+	hough_min_len = src.rows * min_len_factor / 10;
+	cv::HoughLinesP( src_gray, lines, hough_space_resolution, CV_PI/hough_angle_resolution, hough_edge_thresh, hough_min_len, hough_gap);
+	if(lines.size() > 0){
+		Vec4i l = lines[0];
+		line( src_copy, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,255,0), 3, CV_AA);
+	}
 
 	/// Showing the result
 	if(show_frame){
 		resultImage = IplImage(src_gray);
-		sourceImage = IplImage(src);
+		sourceImage = IplImage(src_copy);
 		//skelImage = IplImage(skel);
 		cvShowImage("Result", &resultImage); // show frame
 		cvShowImage("Source", &sourceImage); // show frame
 		//cvShowImage("Skel", &skelImage); // show frame
 	}
 
-	if((float)accu/(src.rows*src.cols) > (float)thresh/100)
+	//if((float)accu/(src.rows*src.cols) > (float)thresh/100)
+	if(lines.size() > 0)
 		return true;
 	return false;
 }
@@ -84,9 +83,10 @@ bool LineToLine::processImg(cv::Mat src){
 void LineToLine::init(){
 	cvNamedWindow("Source",0);
 	cvNamedWindow("Result",0);
-	cvNamedWindow("Skel",0);
+	//cvNamedWindow("Skel",0);
 	cv::createTrackbar( "Blue Dominance: ", "Result", &dominance, 200);
 	cv::createTrackbar( "Threshold: ", "Result", &thresh, 100);
-	//cv::createTrackbar( "Angle Tolerance: ", "Main", &angle_tolerance, 90);
+	cv::createTrackbar( "Angle Resolution: ", "Result", &hough_angle_resolution, hough_angle_resolution_max);
+	cv::createTrackbar( "Length Factor: ", "Result", &min_len_factor, 10);
 }
 
--- src/lineToLine_main.cpp
+++ src/lineToLine_main.cpp
@@ -12,7 +12,7 @@
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include "LineToLine.h".h"
+#include "LineToLine.h"
 
 CvCapture* cv_cap;
 int c = 0;
@@ -21,34 +21,92 @@ cv_bridge::CvImagePtr cv_ptr;
 using namespace cv;
 
 cv::Mat image;
+int miss_accu = 0;
+int miss_max = 5; // maximum number of unexpected, consecutive tracking results before switching to next step.
+std::vector<std::string> step_msgs;
+std::vector<bool> expected_tracking;
 
+//Mat rotateImage(const Mat& source, double angle)
+//{
+//    Point2f src_center(source.cols/2.0F, source.rows/2.0F);
+//    Mat rot_mat = getRotationMatrix2D(src_center, angle, 1.0);
+//    Mat dst;
+//    warpAffine(source, dst, rot_mat, source.size());
+//    return dst;
+//}
 
 
 int main(int argc, char **argv)
 {
+	int step = 0;
+	step_msgs.push_back("Seeing first line. Going forward");
+	step_msgs.push_back("Left first line. Going forward");
+	step_msgs.push_back("Seeing second line. Going forward");
+	step_msgs.push_back("Left second line. Going backward");
+	step_msgs.push_back("Seeing second line again. Going backward");
+	step_msgs.push_back("Left second line again. Going backward");
+	step_msgs.push_back("Seeing first line again. Going backwards");
+	expected_tracking.push_back(true);
+	expected_tracking.push_back(false);
+	expected_tracking.push_back(true);
+	expected_tracking.push_back(false);
+	expected_tracking.push_back(true);
+	expected_tracking.push_back(false);
+	expected_tracking.push_back(true);
 	ros::init(argc, argv, "LineToLine");
-	//ros::NodeHandle node;
-	//image_transport::ImageTransport it(node);
-	//image_transport::Subscriber img_subs = it.subscribe("ardrone/front", 1, imageCallback);
+   // ros::Rate loop_rate(50);
 
-    //ros::Rate loop_rate(50);
 	cv_cap = cvCaptureFromCAM(0);
 	LineToLine* ltl = new LineToLine();
 
+	//image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+	//resize(image, image, Size(image.cols/6, image.rows/6));
+	image = cvQueryFrame(cv_cap);
 
-
-	while (true/*ros::ok()*/) {
-
+	// wait for the user to start
+	printf("Position the drone over the first line and facing to thesecond line, then hit o to go.\n");
+	while(true){
 		image = cvQueryFrame(cv_cap);
-		if(ltl->processImg(image))
-			printf("Detecting something...\n");
-		else
-			printf("Detecting nothing...\n");
+		ltl->processImg(image);
+		c = cvWaitKey(10); // wait 10 ms or for key stroke
+		if(c == 111)
+			break; // if ESC, break and quit
+	}
+
+	// drone has to be positioned over first line and must be detecting the line for this to work
+	while(true){
+		image = cvQueryFrame(cv_cap);
+		if(expected_tracking.at(step) != ltl->processImg(image)){
+			miss_accu++;
+			if(miss_accu == miss_max)
+				step++;
+		}else
+			miss_accu = 0;
+		if(step >= step_msgs.size())
+				break;
+		std::cout << string( 50, '\n' );
+		printf("Step %i. \n%s.\n", step, step_msgs.at(step).c_str());
 		c = cvWaitKey(10); // wait 10 ms or for key stroke
 		if(c == 27)
 			break; // if ESC, break and quit
-		//loop_rate.sleep();
 	}
+	std::cout << string( 50, '\n' );
+	printf("Crossed first line. Stopping and landing.\nPress key to end.\n");
+	cvWaitKey();
+
+//	double angle = 5;
+//	while (true/*ros::ok()*/) {
+//		image = rotateImage(image, angle);
+//		//image = cvQueryFrame(cv_cap);
+//		if(ltl->processImg(image))
+//			printf("Detecting something...\n");
+//		else
+//			printf("Detecting nothing...\n");
+//		c = cvWaitKey(10); // wait 10 ms or for key stroke
+//		if(c == 27)
+//			break; // if ESC, break and quit
+//		//loop_rate.sleep();
+//	}
 
 	/* clean up */
 	cvReleaseCapture( &cv_cap );
